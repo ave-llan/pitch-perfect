7c16c693439d2a36b8c0d8bb1423dbcbac73052f
John Leszczynski <john@bizy.com>
2016-03-01 15:39:38 -0500

    disable record button after touch

--- pitch-perfect/ViewController.swift
+++ pitch-perfect/ViewController.swift
@@ -9,14 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -26,17 +27,19 @@ class ViewController: UIViewController {
         //Hide the stop button
         stopButton.hidden = true
     }
-
+    
     @IBAction func recordAudio(sender: UIButton) {
-      recordingInProgress.hidden = false
-      stopButton.hidden = false
-      print("recording...")
-      // TODO: record audio after touch
+        recordButton.enabled = false
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        print("recording...")
+        // TODO: record audio after touch
     }
-
+    
     @IBAction func stopRecordAudio(sender: UIButton) {
-      recordingInProgress.hidden = true
-      stopButton.hidden = true
+        recordButton.enabled = true
+        recordingInProgress.hidden = true
+        stopButton.hidden = true
     }
 }
 
